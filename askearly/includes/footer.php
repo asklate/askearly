c249135504bd961680ff70b2469709e10a1397ac
Kyle Eggleston <eggs447@gmail.com>
2026-02-07 22:11:32 -0700

    Corrected Directories

---
+++ askearly/includes/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+    <p><span class="caps">AskEarly dot NET</span> is the brain child of Kyle Eggleston Copyright &copy; 2023-<?php echo date("Y");?></p>
+    <p>This site doesn't track cookies, so no worries there. If in the future I decide to do something with cookies, a banner will appear here.</p>
+</div>
+</body>
+</html>
---
+++ askearly/story.php
@@ -0,0 +1,136 @@
+<?php
+$title = "Story";
+$selected = "story";
+;?>
+<?php include_once("includes/header.php");?>
+
+<div class="row">
+    <div class="column">
+        <h2>I am the writer and the dream</h2>
+
+        <p>Ah welcome! Yes welcome indeed. If you know where you are, then you know what this place is.</p>
+        <p>If you do not know where you are? Well then there's a problem now isn't there.</p>
+
+        <h2>Thinking</h2>
+
+        <p><a href="docs/thoughts/security.pdf" target="_blank">Some thoughts on Security</a>
+            Security is important in this day and age, what about document security?</p>
+
+        <p><a href="docs/thoughts/religion.pdf" target="_blank">Religion</a> can mean a lot of things to a
+            lot of different people. This is a look at the LDS church and its teachings.</p>
+
+        <h2>Series</h2>
+
+        <p><a href="/alliances.php">Alliances</a> A series set on an outpost at the edge of
+            explored space by the United Earth Force Alliance.</p>
+    </div>
+    <div class="column">
+        <h2>2026</h2>
+        <p><a href="docs/2026/community.pdf" target="_blank">Community</a>
+            Jessica is at a dead end job going nowhere until she meets some new friends.</p>
+
+        <p><a href="docs/2026/time-travel.pdf" target="_blank">Time Travel</a>
+            Frank and Sam are time travelers. These are some of their experiences.</p>
+
+        <h2>2025</h2>
+        <p><a href="docs/2025/shadows.pdf" target="_blank">Shadows</a>
+            Nothing is ever as it seems.</p>
+
+        <h2>2024</h2>
+        <p><a href="docs/2024/dull-moment.pdf" target="_blank">Never A Dull Moment</a>
+	    Jennifer has a conversation with her future self.</p>
+
+        <p><a href="docs/2024/curtain.pdf" target="_blank">The Man Behind The Curtain</a>
+	    Living alone can cause some kind of fear.</p>
+
+        <h2>2023</h2>
+
+        <p><a href="docs/2023/devil.pdf" target="_blank">Deal With The Devil</a>
+            George goes to Hell but meets the Devil with an idea.</p>
+
+        <h2>2021</h2>
+
+        <p><a href="docs/2021/genie.pdf" target="_blank">Genie</a>
+            Eric finds a genie and sees what happens when she begins granting wishes.</p>
+
+        <p><a href="docs/2021/fold.pdf" target="_blank">Truth In The Fold</a>
+            Aliens and Humans just don't get along.</p>
+
+        <p><a href="docs/2021/station.pdf" target="_blank">The Station</a>
+            A space station on the edge of known space where aliens mingle.</p>
+
+        <p><a href="docs/2021/night.pdf" target="_blank">A Night Out</a>
+            An elephant has a night out.</p>
+
+        <h2>2020</h2>
+
+        <p><a href="docs/2020/cards.pdf" target="_blank">Cards</a>
+            A Magician finds more than he hoped for during an act.</p>
+
+        <h2>2014</h2>
+
+        <p><a href="docs/2014/end-of-earth-3.pdf" target="_blank">End of Earth (Part 3)</a>
+            The conclusion of the End of Earth Series.</p>
+
+        <h2>2013</h2>
+
+        <p><a href="docs/2013/end-of-earth-2.pdf" target="_blank">End of Earth (Part 2)</a>
+            What lies below on the Ice Planet?</p>
+
+        <h2>2012</h2>
+
+        <p><a href="docs/2012/end-of-earth-1.pdf" target="_blank">End of Earth (Part 1)</a>
+            Earth's last moments.</p>
+
+        <h2>2004</h2>
+
+        <p><a href="docs/2004/writers-block-sucks.pdf" target="_blank">Writer's Block Sucks</a>
+            Life as a vampire isn't all that great when you have writers block... or is it?</p>
+
+        <h2>2003</h2>
+
+        <p><a href="docs/2003/only-a-digit-away.pdf" target="_blank">Only A Digit Away</a>
+            Cell phones are the in thing these days... what if that's exactly what they wanted?</p>
+    </div>
+    <div class="column">
+        <h2>Star Trek Fan Fiction</h2>
+
+        <p><a href="docs/fanfic/assimilation.pdf" target="_blank">Assimilation Season</a>
+            What if a borg made it past the events of 2063 and connected with the borg in the future?</a></p>
+
+        <h2>Adventures of Susan The Assassin</h2>
+
+        <dl>
+            <dt><strong>Susan</strong></dt>
+            <dd>
+                <p><a href="docs/susan/susan.pdf" target="_blank">Susan</a> (Drabble)</p>
+            </dd>
+            <dt><strong>The Apartment</strong></dt>
+            <dd>
+                <p><a href="docs/susan/the-cabbie.pdf" target="_blank">The Cabbie</a> (Vignette)</p>
+                <p><a href="docs/susan/the-dead.pdf" target="_blank">The Dead</a> (Drabble)</p>
+                <p><a href="docs/susan/the-bridge.pdf" target="_blank">The Bridge</a> (Drabble)</p>
+            </dd>
+            <dt><strong>Lock Down</strong></dt>
+            <dd>
+                <p><a href="docs/susan/lockdown.pdf" target="_blank">Lock Down</a> (Drabble)</p>
+                <p><a href="docs/susan/knees.pdf" target="_blank">On Her Knees</a> (Drabble)</p>
+                <p><a href="docs/susan/cuffed.pdf" target="_blank">Cuffed</a> (Drabble)</p>
+                <p><a href="docs/susan/the-car-ride.pdf" target="_blank">The Car Ride</a> (Vignette)</p>
+                <p><a href="docs/susan/darkness.pdf" target="_blank">Darkness</a> (Drabble)</p>
+                <p><a href="docs/susan/waiting.pdf" target="_blank">Waiting</a> (Drabble)</p>
+                <p><a href="docs/susan/warehouse.pdf" target="_blank">The Warehouse</a> (Drabble)</p>
+                <p><a href="docs/susan/questions.pdf" target="_blank">Questions</a> (Drabble)</p>
+                <p><a href="docs/susan/dunk-tank.pdf" target="_blank">Dunk Tank</a> (Drabble)</p>
+            </dd>
+            <dt><strong>Misc</strong></dt>
+            <dd>
+                <p><a href="docs/susan/tom.pdf" target="_blank">Tom</a> (Drabble)</p>
+                <p><a href="docs/susan/roxy.pdf" target="_blank">Roxy</a> (Drabble)</p>
+                <p><a href="docs/susan/vanessa.pdf" target="_blank">Vanessa</a> (Vignette)</p>
+            </dd>
+        </dl>
+    </div>
+</div>
+
+<?php include_once("includes/footer.php");?>
