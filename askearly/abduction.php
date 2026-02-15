24763828a31152017c9dfe2152cea4926409caa7
Kyle Eggleston <eggs447@gmail.com>
2026-02-14 20:00:37 -0700

    Added Premise

--- askearly/abduction.php
+++ askearly/abduction.php
@@ -7,6 +7,12 @@ $selected = "story";
 <div class="row">
     <div class="column">
         <h2>Abduction</h2>
+        <p><a href="docs/abduction/premise.pdf" target="_blank">Premise</a>
+            A simple writeup of what this small series is all about. May contain spoilers.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="column">
         <h3>Season One</h3>
         <p>101 <a href="docs/abduction/100/101-pilot.pdf" target="_blank">Pilot</a>
             Joe meets an alien who needs his help to get back home.</p>
