62e0c322a46d5f1dbc812e92a7f182b5834c9aa5
Kyle Eggleston <eggs447@gmail.com>
2026-03-04 18:15:49 -0700

    added seconds to load

--- askearly/includes/footer.php
+++ askearly/includes/footer.php
@@ -1,6 +1,9 @@
 <div class="footer">
     <p><span class="caps">AskEarly dot NET</span> is the brain child of Kyle Eggleston Copyright &copy; 2023-<?php echo date("Y");?></p>
     <p>This site doesn't track cookies, so no worries there. If in the future I decide to do something with cookies, a banner will appear here.</p>
+<?php
+echo "<p>Page loaded in " . number_format(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 4) . " seconds.</p>";
+?>
 </div>
 </body>
 </html>
