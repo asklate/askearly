7f7fbd4a0296918a4be1eb43a86f72544b0f8515
Kyle Eggleston <eggs447@gmail.com>
2026-03-23 18:37:24 -0600

    Added religion files

--- askearly/religion.php
+++ askearly/religion.php
@@ -8,10 +8,64 @@ $selected = "religion";
 
     <div class="column">
         <h2>Religion</h2>
+        <h3>The Core</h3>
         <p><a href="creed.php">Creed</a>: A statement of my beliefs.</p>
         <p><a href="questions.php">Questions</a>: A list of questions regarding the LDS Church.</p>
         <p><a href="docs/thoughts/religion.pdf" target="_blank">Religion</a> can mean a lot of things to a
             lot of different people. This is a look at the LDS church and its teachings.</p>
+        <h3>Talks</h3>
+        <p><a href="docs/religion/talks/agency.pdf" target="_blank">Agency</a></p>
+        <p><a href="docs/religion/talks/christlike-example.pdf" target="_blank">Living a Christ-Like Life &amp; Being a Christ-Like Example for Others</a></p>
+        <p><a href="docs/religion/talks/conversion.pdf" target="_blank">Conversion</a></p>
+        <p><a href="docs/religion/talks/diligent-work.pdf" target="_blank">Diligent Work</a></p>
+        <p><a href="docs/religion/talks/holy-ghost.pdf" target="_blank">The Holy Ghost</a></p>
+        <p><a href="docs/religion/talks/honesty.pdf" target="_blank">Honesty</a></p>
+        <p><a href="docs/religion/talks/repentance.pdf" target="_blank">Repentance</a></p>
+        <p><a href="docs/religion/talks/repentance-2.pdf" target="_blank">Repentance and Testimony</a></p>
+        <p><a href="docs/religion/talks/sabbath-day.pdf" target="_blank">The Sabbath Day</a></p>
+        <p><a href="docs/religion/talks/some-thoughts-on-topics.pdf" target="_blank">Some Thoughts On Topics</a></p>
+    </div>
+    <div class="column">
+        <h3>2018</h3>
+        <p><a href="docs/religion/2018/09/a-thought-process-of-nothing.pdf" target="_blank">A thought Process Of Nothing</a></p>
+        <p><a href="docs/religion/2018/09/articles-of-faith-comparison.pdf" target="_blank">Articles of Faith</a></p>
+        <p><a href="docs/religion/2018/09/articles-of-faith-revised.pdf" target="_blank">Articles of Faith Revised</a></p>
+        <p><a href="docs/religion/2018/09/be-you.pdf" target="_blank">Be You</a></p>
+        <p><a href="docs/religion/2018/09/death.pdf" target="_blank">Death</a></p>
+        <p><a href="docs/religion/2018/09/morals.pdf" target="_blank">A Shower Thought: I can handle the history, but the morals I don’t Understand</a></p>
+        <p><a href="docs/religion/2018/09/only-a-dream.pdf" target="_blank">It Is Only A Dream</a></p>
+        <p><a href="docs/religion/2018/09/pre-existence.pdf" target="_blank">Pre-Existence</a></p>
+        <p><a href="docs/religion/2018/10/aof01.pdf" target="_blank">Article of Faith 1</a></p>
+        <p><a href="docs/religion/2018/10/aof06.pdf" target="_blank">Article of Faith 6</a></p>
+        <p><a href="docs/religion/2018/10/aof11.pdf" target="_blank">Article of Faith 11</a></p>
+        <p><a href="docs/religion/2018/10/Blackness.pdf" target="_blank">Curse of Blackness</a></p>
+        <p><a href="docs/religion/2018/10/changes.pdf" target="_blank">Changes Over The Years</a></p>
+        <p><a href="docs/religion/2018/10/church.pdf" target="_blank">Thus Shall My Church Be Called</a></p>
+        <p><a href="docs/religion/2018/10/opposition.pdf" target="_blank">An Opposition In All Things</a></p>
+        <p><a href="docs/religion/2018/10/quandary.pdf" target="_blank">Here’s an interesting.... Quandary</a></p>
+        <p><a href="docs/religion/2018/10/speaking.pdf" target="_blank">Speaking out against the Church</a></p>
+        <p><a href="docs/religion/2018/10/style-guide.pdf" target="_blank">Style Guide — The Name of the Church</a></p>
+        <p><a href="docs/religion/2018/10/swedish.pdf" target="_blank">Swedish Rescue</a></p>
+        <p><a href="docs/religion/2018/10/the-ocean.pdf" target="_blank">The Ocean</a></p>
+        <p><a href="docs/religion/2018/10/what-exactly-is-truth.pdf" target="_blank">What Exactly Is Truth?</a></p>
+        <p><a href="docs/religion/2018/11/astray.pdf" target="_blank">The Prophet Won’t Lead The People Astray</a></p>
+        <p><a href="docs/religion/2018/11/blood.pdf" target="_blank">Blood Changes</a></p>
+        <p><a href="docs/religion/2018/11/faith.pdf" target="_blank">Fear not: believe only</a></p>
+        <p><a href="docs/religion/2018/11/forgiveness.pdf" target="_blank">Forgiveness</a></p>
+        <p><a href="docs/religion/2018/11/fullness.pdf" target="_blank">Alternating Messages</a></p>
+        <p><a href="docs/religion/2018/11/tithing.pdf" target="_blank">Tithing</a></p>
+        <p><a href="docs/religion/2018/12/moses-bible-comparison.pdf" target="_blank">Moses and Bible Comparisons</a></p>
+        <p><a href="docs/religion/2018/12/two-churches.pdf" target="_blank">Two Churches</a></p>
+        <p><a href="docs/religion/2018/12/what-matters-most-is-whats--in-your-heart.pdf" target="_blank">What Matters Most Is What's In Your Heart</a></p>
+        <p><a href="docs/religion/2018/12/wicked.pdf" target="_blank">Wicked People Are All Around</a></p>
+        <h3>2019</h3>
+        <p><a href="docs/religion/2019/01/aof2.pdf" target="_blank">Article of Faith 2</a></p>
+        <p><a href="docs/religion/2019/01/doctrine.pdf" target="_blank">What is Doctrine</a></p>
+        <p><a href="docs/religion/2019/01/vitamins.pdf" target="_blank">Eat Your Vitamins</a></p>
+        <p><a href="docs/religion/2019/04/nov2015Policy.pdf" target="_blank">Policy Changes</a></p>
+        <p><a href="docs/religion/2019/04/scripture-comparison.pdf" target="_blank">Comparisons Between BOM and Bible Scriptures</a></p>
+        <h3>2020</h3>
+        <p><a href="docs/religion/2020/04/the-law.pdf" target=="_blank">The Law</a></p>
     </div>
 
 </div>
