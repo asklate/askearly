e8631809075c2affc7071549d1c9dfd55f1834de
Kyle Eggleston <eggs447@gmail.com>
2026-04-01 17:20:26 -0600

    Added boise rescue

--- askearly/religion.php
+++ askearly/religion.php
@@ -29,6 +29,7 @@ $selected = "religion";
         <p><a href="docs/religion/sources/emma-smith.pdf" target="_blank">Last Testimony of Sister Emma</a></p>
         <p><a href="docs/religion/sources/swedish-rescue-2010.pdf" target="_blank">2010 Sweden Fireside (Swedish Rescue)</a></p>
         <p><a href="docs/religion/sources/swedish-rescue-2018.pdf" target="_blank">2018 Sweden Fireside (Swedish Rescue Follow-up?)</a></p>
+        <p><a href="docs/religion/sources/boise-rescue-2015.pdf" target="_blank">2015 Boise Fireside (Boise Rescue)</a></p>
     </div>
     <div class="column">
         <h3>2018</h3>
