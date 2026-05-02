f13af1bdb04ab8a7fa4506642657c1f20840306b
Kyle Eggleston <eggs447@gmail.com>
2026-05-01 21:27:23 -0600

    Wentworth Letter

--- askearly/religion.php
+++ askearly/religion.php
@@ -31,6 +31,7 @@ $selected = "religion";
         <p><a href="docs/religion/sources/swedish-rescue-2018.pdf" target="_blank">2018 Sweden Fireside (Swedish Rescue Follow-up?)</a></p>
         <p><a href="docs/religion/sources/boise-rescue-2015.pdf" target="_blank">2015 Boise Fireside (Boise Rescue)</a></p>
         <p><a href="docs/religion/sources/fourteen-articles-of-faith.pdf" target="_blank">Fourteen Articles of Faith</a></p>
+        <p><a href="docs/religion/sources/wentworth-letter.pdf" target="_blank">The Wentworth Letter</a></p>
     </div>
     <div class="column">
         <h3>2018</h3>
